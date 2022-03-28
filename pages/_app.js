86b2a815fe221d4ef6425da6d2b0fb507ca42d9b
Thinushi <thinupavani@gmail.com>
2022-03-28 23:01:12 +0530

    Navbar structure created

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Structure from "../components";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Structure>
+      <Component {...pageProps} />
+    </Structure>
+  );
 }
 
 export default MyApp;
