5cf5f06894a2d74602cfdc0775429e5b964630f4
Thinushi <thinupavani@gmail.com>
2022-03-28 23:02:43 +0530

    Landing UI main section structure created

--- components/layoutComponents/LandingUI.jsx
+++ components/layoutComponents/LandingUI.jsx
@@ -3,7 +3,23 @@ import React from "react";
 const LandingUI = () => {
   return (
     <>
-      <h1>Landing</h1>
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-md-7 col-sm-12">
+            <h6>AUTHOR: DAILY TUITION</h6>
+            <h1>EXITING ADVENTURE</h1>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit
+              omnis asperiores aliquid minus labore accusamus natus, quos velit
+              exercitationem aut?
+            </p>
+            <button class="btn btn-light px-5 py-2"> Buy Now</button>
+          </div>
+          <div class="col-md-5 col-sm-12 h-25">
+            <img src="../../images/teaLeaves.png" alt="tea" />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
