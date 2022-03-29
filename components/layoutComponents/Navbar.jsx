1d193abca36fceff1eef56559a723ac19042a83d
Thinushi <thinupavani@gmail.com>
2022-03-29 13:03:03 +0530

    Navbar styles added

--- components/layoutComponents/Navbar.jsx
+++ components/layoutComponents/Navbar.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import styles from "../../styles/Navbar.module.css";
 
 const Navbar = () => {
   return (
     <>
       <nav class="navbar navbar-expand-lg sticky-top">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            XYZteas
+          <a class="navbar-brand" href="#" className={styles.navbarBrand}>
+            <img
+              src="../../images/logo.png"
+              alt="XYZ teas logo"
+              className={styles.logo}
+            />
           </a>
           <button
             class="navbar-toggler"
@@ -22,19 +27,34 @@ const Navbar = () => {
           <div class="collapse navbar-collapse" id="navbarNav">
             <div class="mx-auto"></div>
             <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
-                  Home
+              <li class="nav-item" className={styles.navItem}>
+                <a
+                  class="nav-link active"
+                  aria-current="page"
+                  href="#"
+                  className={styles.navbarLink}
+                >
+                  HOME
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Features
+              <li class="nav-item" className={styles.navItem}>
+                <a class="nav-link" href="#" className={styles.navbarLink}>
+                  PRODUCTS
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Pricing
+              <li class="nav-item" className={styles.navItem}>
+                <a class="nav-link" href="#" className={styles.navbarLink}>
+                  OUR SERVICES
+                </a>
+              </li>
+              <li class="nav-item" className={styles.navItem}>
+                <a class="nav-link" href="#" className={styles.navbarLink}>
+                  ABOUT
+                </a>
+              </li>
+              <li class="nav-item" className={styles.navItem}>
+                <a class="nav-link" href="#" className={styles.navbarLink}>
+                  CONTACT
                 </a>
               </li>
             </ul>
