1698a8ec6cd053413eebe2ad466955e9df76d57b
Thinushi <thinupavani@gmail.com>
2022-03-29 23:47:06 +0530

    Content changed and img converted to next Image

--- components/layoutComponents/LandingUI.jsx
+++ components/layoutComponents/LandingUI.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const LandingUI = () => {
@@ -6,16 +7,23 @@ const LandingUI = () => {
       <div className="container text-center">
         <div className="row">
           <div className="col-md-7 col-sm-12 justify-content-center">
-            <h6>AUTHOR: DAILY TUITION</h6>
-            <h1>EXITING ADVENTURE</h1>
-            <p>
+            <h6 style={{ fontWeight: "300" }}>
+              Over 60 years in the tea industry
+            </h6>
+            <h1 style={{ fontSize: "4.5em" }}>EXITING ADVENTURE</h1>
+            <p style={{ fontWeight: "300" }}>
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit
               omnis asperiores aliquid minus labore accusamus natus, quos velit
               exercitationem aut?
             </p>
           </div>
           <div className="col-md-5 col-sm-12 h-25">
-            <img src="../../images/teaLeaves.png" alt="tea" />
+            <Image
+              src="/images/teaLeaves.png"
+              alt="tea"
+              width={400}
+              height={600}
+            />
           </div>
         </div>
       </div>
