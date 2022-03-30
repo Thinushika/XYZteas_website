38b0c55d17c0ab23f4c01b8ecf067d59a214c75a
Thinushi <thinupavani@gmail.com>
2022-03-30 18:58:08 +0530

    product images imported and style changed

--- components/layoutComponents/Products.jsx
+++ components/layoutComponents/Products.jsx
@@ -6,12 +6,7 @@ const Cards = () => {
     <>
       <div className="cover">
         <div className="container text-center">
-          <h1 style={{ margin: "0.5em" }}>Our Products</h1>
-          <p style={{ margin: "0.5em" }}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit omnis
-            asperiores aliquid minus labore accusamus natus, quos velit
-            exercitationem aut?
-          </p>
+          <h1>Our Products</h1>
         </div>
       </div>
       <div className="container-fluid text-center">
@@ -19,18 +14,30 @@ const Cards = () => {
           {data.map((post) => {
             return (
               <div
-                className="card"
-                style={{ width: "18rem", margin: "1em" }}
+                className="card mt-3 mb-5"
+                style={{
+                  width: "18rem",
+                  marginInline: "1em",
+                  borderRadius: "10px",
+                  borderStyle: "none",
+                  boxShadow: "0 8px 16px 0 rgba(0,0,0,0.2)",
+                }}
                 key={post.id}
               >
                 <img
                   className="card-img-top"
-                  src="../../images/logo.png"
+                  src={post.image}
                   alt="Card image cap"
+                  height="350px"
                 />
                 <div className="card-body">
                   <h4>{post.title}</h4>
-                  <p className="card-text">{post.desc}</p>
+                  <p
+                    className="card-text"
+                    style={{ fontWeight: "300", color: "#888" }}
+                  >
+                    {post.desc}
+                  </p>
                 </div>
               </div>
             );
