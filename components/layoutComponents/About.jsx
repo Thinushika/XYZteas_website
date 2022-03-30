a67de08910d3c5ba48a1e086615b8e086455a0d1
Thinushi <thinupavani@gmail.com>
2022-03-30 15:32:21 +0530

    Design changed - video added, content

--- components/layoutComponents/About.jsx
+++ components/layoutComponents/About.jsx
@@ -3,28 +3,70 @@ import React from "react";
 const About = () => {
   return (
     <>
-      <div className="container text-center">
+      <div className="container-fluid">
         <div className="row">
-          <div className="col-md-6">
-            <img src="../../images/teaLeaves.png" alt="tea" />
+          <div
+            className="col-md-4 col-sm-12 text-left p-5"
+            style={{
+              backgroundColor: "#141414",
+              color: "#bbb",
+              fontWeight: "200",
+            }}
+          >
+            <h1
+              style={{
+                fontWeight: "600",
+                letterSpacing: "0.07em",
+              }}
+            >
+              We've Been Exporting For Over
+              <span
+                style={{
+                  paddingLeft: "0.5em",
+                  color: "#00ff00",
+                }}
+              >
+                18 Years
+              </span>
+            </h1>
+            <p
+              className="pt-4"
+              style={{
+                fontWeight: "200",
+              }}
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Laboriosam earum quidem assumenda ab, voluptatem perferendis
+              pariatur reiciendis?
+            </p>
+            <p
+              className="pt-4"
+              style={{
+                fontWeight: "200",
+              }}
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Laboriosam earum quidem assumenda ab, voluptatem perferendis
+              pariatur reiciendis?
+            </p>
           </div>
-          <div className="col-md-6">
-            <div className="panel text-left">
-              <h1>XYZ teas</h1>
-              <p className="pt-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Laboriosam earum quidem assumenda ab, voluptatem perferendis
-                pariatur reiciendis? Nemo officiis quibusdam earum neque
-                voluptatum corrupti, fuga quam, corporis, veritatis distinctio
-                saepe.
-              </p>
-              <p className="pt-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Laboriosam earum quidem assumenda ab, voluptatem perferendis
-                pariatur reiciendis? Nemo officiis quibusdam earum neque
-                voluptatum corrupti, fuga quam, corporis, veritatis distinctio
-                saepe.
-              </p>
+          <div
+            className="col-md-8 col-sm-12 p-0"
+            style={{ backgroundColor: "#141414" }}
+          >
+            <div
+              className="overlay"
+              style={{
+                position: "relative",
+                width: "100%",
+                backgroundColor: "#000",
+                opacity: "0.5",
+                zIndex: "1",
+              }}
+            >
+              <video autoPlay muted loop width="100%">
+                <source src="/videos/teaPlantation.mp4" type="video/mp4" />
+              </video>
             </div>
           </div>
         </div>
