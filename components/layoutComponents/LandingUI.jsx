b0e67b725a5e1402d61db46d5d1ddd5a1eec25d0
Thinushi <thinupavani@gmail.com>
2022-03-30 22:35:09 +0530

    UI design changed

--- components/layoutComponents/LandingUI.jsx
+++ components/layoutComponents/LandingUI.jsx
@@ -4,29 +4,54 @@ import React from "react";
 const LandingUI = () => {
   return (
     <>
-      <div className="container text-center">
-        <div className="row">
-          <div className="col-md-7 col-sm-12 justify-content-center">
-            <h6 style={{ fontWeight: "300" }}>
-              Over 60 years in the tea industry
-            </h6>
-            <h1 style={{ fontSize: "4.5em" }}>EXITING ADVENTURE</h1>
-            <p style={{ fontWeight: "300" }}>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit
-              omnis asperiores aliquid minus labore accusamus natus, quos velit
-              exercitationem aut?
-            </p>
-          </div>
-          <div className="col-md-5 col-sm-12 h-25">
+      <div
+        className="container-fluid text-center"
+        style={{
+          backgroundImage:
+            "linear-gradient(90deg,rgba(0, 0, 0, 0.55),rgba(0, 0, 0, 0.55)),url(/images/XYZteasBackground1.jpg)",
+          backgroundSize: "cover",
+          width: "100vw",
+          height: "100vh",
+          justifyContent: "center",
+          alignItems: "center",
+          display: "flex",
+        }}
+      >
+        <div
+          className="container"
+          style={{
+            width: "60%",
+            height: "40%",
+            justifyContent: "center",
+            alignItems: "center",
+            display: "flex",
+            flexDirection: "column",
+            borderRadius: "20px",
+            backgroundImage:
+              "linear-gradient(90deg,rgba(0, 0, 0, 0.55),rgba(0, 0, 0, 0.55))",
+          }}
+        >
+          <h6 style={{ fontWeight: "300", color: "whitesmoke" }}>
+            Over 60 years in the tea industry
+          </h6>
+          <h1 style={{ fontSize: "4.5em", color: "whitesmoke" }}>
+            EXITING ADVENTURE
+          </h1>
+          <p style={{ fontWeight: "300", color: "whitesmoke" }}>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit omnis
+            asperiores aliquid minus labore accusamus natus, quos velit
+            exercitationem aut?
+          </p>
+        </div>
+      </div>
+      {/* <div className="col-md-5 col-sm-12 h-25">
             <Image
               src="/images/teaLeaves.png"
               alt="tea"
               width={400}
               height={600}
             />
-          </div>
-        </div>
-      </div>
+          </div> */}
     </>
   );
 };
