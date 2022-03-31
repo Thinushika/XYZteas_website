22efb0adb84ee5b1273316eb5890a638c920c276
Thinushi <thinupavani@gmail.com>
2022-03-31 13:36:08 +0530

    responsive and designed changed

--- components/layoutComponents/LandingUI.jsx
+++ components/layoutComponents/LandingUI.jsx
@@ -1,14 +1,13 @@
-import Image from "next/image";
 import React from "react";
 
 const LandingUI = () => {
   return (
     <>
       <div
-        className="container-fluid text-center"
+        className="container-fluid text-center px-2 py-6"
         style={{
           backgroundImage:
-            "linear-gradient(90deg,rgba(0, 0, 0, 0.55),rgba(0, 0, 0, 0.55)),url(/images/XYZteasBackground1.jpg)",
+            "linear-gradient(90deg,rgba(153, 155, 133, 0.55),rgba(153, 155, 13, 0.55)),url(/images/XYZteasBackground1.jpg)",
           backgroundSize: "cover",
           width: "100vw",
           height: "100vh",
@@ -18,10 +17,8 @@ const LandingUI = () => {
         }}
       >
         <div
-          className="container"
+          className="container p-4 m-2 my-5"
           style={{
-            width: "60%",
-            height: "40%",
             justifyContent: "center",
             alignItems: "center",
             display: "flex",
@@ -34,13 +31,25 @@ const LandingUI = () => {
           <h6 style={{ fontWeight: "300", color: "whitesmoke" }}>
             Over 60 years in the tea industry
           </h6>
-          <h1 style={{ fontSize: "4.5em", color: "whitesmoke" }}>
-            EXITING ADVENTURE
+          <h1
+            style={{
+              fontSize: "4em",
+              color: "whitesmoke",
+              lineHeight: "0.9em",
+            }}
+          >
+            THE BEST <br /> QUALITY TEA
           </h1>
-          <p style={{ fontWeight: "300", color: "whitesmoke" }}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit omnis
-            asperiores aliquid minus labore accusamus natus, quos velit
-            exercitationem aut?
+          <p
+            style={{
+              fontWeight: "300",
+              color: "whitesmoke",
+              fontSize: "0.9em",
+            }}
+          >
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit
+            <br /> omnis asperiores aliquid minus labore accusamus natus,
+            <br /> quos velit exercitationem aut?
           </p>
         </div>
       </div>
